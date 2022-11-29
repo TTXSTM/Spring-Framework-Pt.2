4e54bb55a49232dae37bdeb84c19a687d45173c4
TTXSTM <71548215+TTXSTM@users.noreply.github.com>
2022-11-29 23:11:58 +0300

    Annotations @Qualifier

--- SpringFramework/src/main/java/space/mairi/springcourse/RockMusic.java
+++ SpringFramework/src/main/java/space/mairi/springcourse/RockMusic.java
@@ -2,10 +2,22 @@ package space.mairi.springcourse;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class RockMusic implements Music {
+
+    private List<String> songs = new ArrayList<String>();
+
+    {
+        songs.add("Hungarian Rhapsody");
+        songs.add("Toccata and Fugue in D minor");
+        songs.add("Symphony No.5 in C minor");
+    }
+
     @Override
-    public String getSong() {
-        return "Wind cries Mary";
+    public List<String> getSong() {
+       return songs;
     }
 }
